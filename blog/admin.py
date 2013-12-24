5be3b486ab3a0a91dc16bfb9edc8a398b3eea55b
badhashsolution <badhashsolution@gmail.com>
2013-12-24 05:02:30 -0800

    working - added blog to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from blog.models import Post # WGG 12/24/2013 5:00 AM - connecting blog app
 
-# Register your models here.
+admin.site.register(Post) # WGG 12/24/2013 5:00 AM - connecting blog app in admin
